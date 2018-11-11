b9ee8215ad08c28ea3cfb4177534d11ede55dd57
Ntokozo Majozi <ntokozo@sov.tech>
2018-11-11 08:55:18 +0200

    reg screen addition

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { StackNavigator } from 'react-navigation'
+import RegistrationScreen from '../Containers/RegistrationScreen'
 import HomeScreen from '../Containers/HomeScreen'
 import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
@@ -7,6 +8,7 @@ import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  RegistrationScreen: { screen: RegistrationScreen },
   HomeScreen: { screen: HomeScreen },
   LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen }
